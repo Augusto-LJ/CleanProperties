cbf64dd26802919af225a8cc5436f86d8f1449b2
Augusto-LJ <augustolimajardim@gmail.com>
2026-03-13 21:38:27 -0300

    feat: creates response wrapper for global responses

---
+++ Application/Wrappers/IResponseWrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Wrappers;
+public interface IResponseWrapper
+{
+    public List<string> Messages { get; set; }
+    public bool IsSuccessful { get; set; }
+}
+
+public interface IResponseWrapper<out T> : IResponseWrapper
+{
+    public T Data { get; }
+}
