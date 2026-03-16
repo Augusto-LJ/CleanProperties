246898954c88ecd3239966eb9d57bbbab678a150
Augusto-LJ <augustolimajardim@gmail.com>
2026-03-15 21:31:31 -0300

    feat: implements delete and update agent commands

---
+++ Application/Features/Agents/Commands/DeleteAgentCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Agents.Commands;
+public class DeleteAgentCommand : IRequest<IResponseWrapper>
+{
+    public int AgentId { get; set; }
+}
+
+public class DeleteAgentCommandHandler(IAgentService _agentService) : IRequestHandler<DeleteAgentCommand, IResponseWrapper>
+{
+    public async Task<IResponseWrapper> Handle(DeleteAgentCommand request, CancellationToken cancellationToken)
+    {
+        var agentId = await _agentService.DeleteAsync(request.AgentId);
+
+        return ResponseWrapper<int>.Success(data: agentId, message: "Agent deleted successfully.");
+    }
+}
---
+++ Application/Features/Agents/Commands/UpdateAgentCommand.cs
@@ -0,0 +1,26 @@
+﻿using Application.Models.Requests;
+using Application.Models.Responses;
+using Application.Wrappers;
+using Domain.Entities;
+using Mapster;
+using MediatR;
+
+namespace Application.Features.Agents.Commands;
+public class UpdateAgentCommand : IRequest<IResponseWrapper>
+{
+    public UpdateAgentRequest UpdateAgent { get; set; }
+}
+
+public class UpdateAgentCommandHandler(IAgentService agentService) : IRequestHandler<UpdateAgentCommand, IResponseWrapper>
+{
+    private readonly IAgentService _agentService = agentService;
+
+    public async Task<IResponseWrapper> Handle(UpdateAgentCommand request, CancellationToken cancellationToken)
+    {
+        var agent = request.UpdateAgent.Adapt<Agent>();
+
+        var updatedAgent = await _agentService.UpdateAsync(agent);
+
+        return ResponseWrapper<AgentResponse>.Success(data: updatedAgent.Adapt<AgentResponse>(), message: "Agent updated successfully.");
+    }
+}
