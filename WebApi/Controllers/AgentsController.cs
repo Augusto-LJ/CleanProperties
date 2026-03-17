ac7d932da24e8591758220b1c303239cff4e8a81
Augusto-LJ <augustolimajardim@gmail.com>
2026-03-17 18:07:37 -0300

    feat: adds BaseController and two AgentsController endpoints

---
+++ WebApi/Controllers/AgentsController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Agents.Commands;
+using Application.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+[Route("api/[controller]")]
+public class AgentsController : BaseController
+{
+    [HttpPost("add")]
+    public async Task<IActionResult> CreateAgentAsync([FromBody] CreateAgentRequest createAgent)
+    {
+        var response = await Sender.Send(new CreateAgentCommand { CreateAgent = createAgent});
+
+        if (response.IsSuccessful)
+            return Ok(response);
+
+        return BadRequest(response);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAgentAsync([FromBody] UpdateAgentRequest updateAgent)
+    {
+        var response = await Sender.Send(new UpdateAgentCommand { UpdateAgent = updateAgent });
+
+        if (response.IsSuccessful)
+            return Ok(response);
+
+        return BadRequest(response);
+    }
+}
---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+[ApiController]
+public class BaseController : ControllerBase
+{
+    private ISender _sender = null;
+    public ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>(); // Lazy initialization
+}
