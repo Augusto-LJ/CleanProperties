4bfc00a03a3739bc6322684f5ec87593f06ff696
Augusto-LJ <augustolimajardim@gmail.com>
2026-03-18 14:54:07 -0300

    feat: implements property service

---
+++ Application/Features/Properties/IPropertyService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Features.Properties;
+public interface IPropertyService
+{
+    Task<int> CreateAsync(Property newProperty);
+    Task<Property> UpdateAsync(Property updateProperty);
+    Task<int> DeleteAsync(int id);
+    Task<Property> GetByIdAsync(int id);
+    Task<List<Property>> GetAllAsync();
+    Task<bool> DoesExistAsync(int id);
+}
