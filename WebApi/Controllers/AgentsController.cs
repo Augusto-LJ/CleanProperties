47eff49a3e7f8d4c8ac9705cc1a3e90095aaf30d
Augusto-LJ <augustolimajardim@gmail.com>
2026-03-18 14:22:53 -0300

    feat: adds remaining agents endpoints

--- WebApi/Controllers/AgentsController.cs
+++ WebApi/Controllers/AgentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Agents.Commands;
+using Application.Features.Agents.Queries;
 using Application.Models.Requests;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +10,7 @@ public class AgentsController : BaseController
     [HttpPost("add")]
     public async Task<IActionResult> CreateAgentAsync([FromBody] CreateAgentRequest createAgent)
     {
-        var response = await Sender.Send(new CreateAgentCommand { CreateAgent = createAgent});
+        var response = await Sender.Send(new CreateAgentCommand { CreateAgent = createAgent });
 
         if (response.IsSuccessful)
             return Ok(response);
@@ -21,6 +22,39 @@ public class AgentsController : BaseController
     public async Task<IActionResult> UpdateAgentAsync([FromBody] UpdateAgentRequest updateAgent)
     {
         var response = await Sender.Send(new UpdateAgentCommand { UpdateAgent = updateAgent });
+
+        if (response.IsSuccessful)
+            return Ok(response);
+
+        return BadRequest(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAgentAsync(int id)
+    {
+        var response = await Sender.Send(new DeleteAgentCommand { AgentId = id });
+
+        if (response.IsSuccessful)
+            return Ok(response);
+
+        return BadRequest(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAgentByIdAsync(int id)
+    {
+        var response = await Sender.Send(new GetAgentByIdQuery { AgentId = id });
+
+        if (response.IsSuccessful)
+            return Ok(response);
+
+        return BadRequest(response);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllAgentsAsync()
+    {
+        var response = await Sender.Send(new GetAllAgentsQuery());
 
         if (response.IsSuccessful)
             return Ok(response);
